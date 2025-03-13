e4f7d6f9b2d5b7f8d09cd3138f3133dec5dc8c41
robert375 <58269632+robert375@users.noreply.github.com>
2025-03-13 15:24:53 +0100

    Add files via upload

---
+++ domasna/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+
+namespace domasna
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num1 = 10;
+            int num2 = 15;
+            int num3 = num1 + num2;
+            Console.WriteLine(num3);
+
+
+
+
+
+
+            int ave1 = 10;
+            int ave2 = 15;
+            int ave3 = 20;
+            int ave4 = 30;
+
+            int avg =( ave1 + ave2 + ave3 + ave4) / 4;
+            Console.WriteLine(avg);
+
+
+            int firstNum = 5;
+            int secondNum = 8;
+
+            (firstNum, secondNum) = (secondNum, firstNum);
+            Console.WriteLine($"firstNum={firstNum} secondNum={secondNum}");
+            
+
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
