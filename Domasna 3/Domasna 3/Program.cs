a6009fc32bd53ea2c580004edc074384ae8edf0b
robert375 <58269632+robert375@users.noreply.github.com>
2025-03-13 15:26:31 +0100

    Add files via upload

---
+++ Domasna 3/Domasna 3/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Domasna_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            
+        {
+
+            
+            string stringDate = "1997-6-18";
+
+            DateTime convertDate = DateTime.Parse(stringDate);
+            Console.WriteLine(stringDate);
+            DateTime currentDay = DateTime.Today;
+            Console.WriteLine(currentDay);
+
+            Console.WriteLine(convertDate.Month);
+
+
+
+
+           
+
+
+            static int AgeCalculator(DateTime birthDate)
+            {
+                int age = DateTime.Today.Year - birthDate.Year;
+                return age;
+                    }
+
+
+                    Console.WriteLine("Hello, World!");
+
+
+
+        }
+    }
+}
