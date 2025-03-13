4749c997051f80d892e258fe7fb69c392a0fd324
robert375 <58269632+robert375@users.noreply.github.com>
2025-03-13 15:25:49 +0100

    Add files via upload

---
+++ Domasna 2/Domasna 2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Channels;
+
+namespace Domasna_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sumOfEven = new int[6];
+            for (int i = 0; i < sumOfEven.Length; i++)
+                
+                { 
+                string input = Console.ReadLine();
+                int number = int.Parse(input);
+                sumOfEven[i] = number;
+
+            }
+
+            {
+
+                int sum = 0;
+                foreach (int number in sumOfEven)
+                    if (number % 2 == 0)
+                    {
+                        sum += number;
+                    }else
+
+                    {
+                        Console.WriteLine("please enter even number");
+                    }
+                        Console.WriteLine(sum);
+                Console.WriteLine("Hello, World!");
+            }
+        }
+    }
+}
